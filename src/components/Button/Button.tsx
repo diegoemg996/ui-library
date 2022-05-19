79bf5d54aebb074d069887cc7bdfdfa5dcfc4de2
diego <diegoemg996@gmail.com>
2022-05-19 08:58:24 -0500

    cambio prueba

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { FC } from "react";
+
 
 export interface ButtonProps {
   label: string;
+  onClick: () => void;
+  
+
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button : FC<ButtonProps> = ({label, ...props}) => {
+  return( 
+    <button {...props}>
+      {label}
+    </button>);
 };
 
 export default Button;
