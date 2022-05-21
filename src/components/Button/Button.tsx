735125a1ea3a4414b1cc1befb25ec8fe31ef55fe
diego <diegoemg996@gmail.com>
2022-05-21 11:27:29 -0500

    sorybooks added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from "react";
+import './Button.css';
 
 
 export interface ButtonProps {
   label: string;
-  onClick: () => void;
-  
-
 }
+
 
 const Button : FC<ButtonProps> = ({label, ...props}) => {
   return( 
